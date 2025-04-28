6680685f6695f465ff04c1ec64eeb32bdae2927f
gaetanopyro <gaetano.miscio@gmail.com>
2025-04-28 17:58:27 +0200

    add exercise 1

---
+++ assets/js/style.js
@@ -0,0 +1,24 @@
+class User {
+  constructor(_firstName, _lastName, _age, _location) {
+    this.firstName = _firstName;
+    this.lastName = _lastName;
+    this.age = _age;
+    this.location = _location;
+  }
+  compareAge(user) {
+    if (this.age > user.age) {
+      return this.firstName + "è piu grande " + user.firstName;
+    } else if (this.age < user.age) {
+      return this.firstName + "è piu piccolo di " + user.firstName;
+    } else {
+      return "hanno la stessa età";
+    }
+  }
+}
+const rossi = new User("Mario", "Rossi", 30, "Veneto");
+console.log(rossi);
+
+const verdi = new User("Giuseppe", "Verdi", 40, "Lombardia");
+console.log(verdi);
+
+console.log(rossi.compareAge(verdi));
